9f3d83915482bd3cfd87cd7f219ab911eb8821e1
강나연 <skdusdl4@naver.com>
2025-01-07 17:56:41 +0900

    [add]

--- validation.js
+++ validation.js
@@ -1,6 +1,53 @@
 // validation.js
 
 export const isEmail = (value) => {
-    // value가 이메일 형식에 맞으면 true, 형식에 맞지 않으면 false를 return 하도록 구현해보세요
-    return false;
+    const email = value || "";
+    const [localPart, domain, ...etc] = email.split("@");
+  
+    if (!localPart || !domain || etc.length) {
+      return false;
+    } else if (email.includes(" ")) {
+      return false;
+    } else if (email[0] === "-") {
+      return false;
+    }
+  
+    for (const word of localPart.toLowerCase().split("")) {
+      if (!["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9","-","+","_"].includes(word)) {
+        return false;
+      }
+    }
+  
+    for (const word of domain.toLowerCase().split("")) {
+      if (!["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9","-", "."].includes(word)) {
+        return false;
+      }
+    }
+  
+    return true;
   };
+
+
+
+  // validation.js
+
+  // 정규 표현식 예시
+
+// export const isEmail = (value) => {
+//     const email = value || "";
+//     const [localPart, domain, ...etc] = email.split("@");
+  
+//     if (!localPart || !domain || etc.length) {
+//       return false;
+//     } else if (email.includes(" ")) {
+//       return false;
+//     } else if (email[0] === "-") {
+//       return false;
+//     } else if (!/^[a-z0-9+_-]+$/gi.test(localPart)) {
+//       return false;
+//     } else if (!/^[a-z0-9.-]+$/gi.test(domain)) {
+//       return false;
+//     }
+  
+//     return true;
+//   };
