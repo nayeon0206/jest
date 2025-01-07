c553d49e2b42501bb577b9cbf232f156c106641a
강나연 <skdusdl4@naver.com>
2025-01-07 17:38:51 +0900

    Create validation.spec.js

---
+++ validation.spec.js
@@ -0,0 +1,11 @@
+// validation.spec.js
+
+import { isEmail } from './validation';
+
+test('테스트가 성공하는 상황', () => {
+  expect(isEmail('이메일이 아니에요')).toEqual(false);
+});
+
+test('테스트가 실패하는 상황', () => {
+  expect(isEmail('my-email@domain.com')).toEqual(true);
+});
